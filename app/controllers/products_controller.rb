63effc123dab9880e58fde8ff1ae37cd009ae1cd
Giordano Dìaz <diazgiordano@gmail.com>
2021-10-18 15:34:40 -0500

    Add new content to show page and new page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all.includes(:variants)
     respond_to do |format|
@@ -13,6 +15,9 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def edit
+  end
   
 
   def create
@@ -24,7 +29,31 @@ class ProductsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @product.update(products_params)
+        format.html { redirect_to @product, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: 'Post was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def products_params
     params.require(:product).permit(:name, :description, :variants)
